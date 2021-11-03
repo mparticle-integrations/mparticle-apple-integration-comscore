6a508fa327ef5a7e50566c2664231ec156d0c8df
Breton Goers <breton.goers@disney.com>
2021-11-03 15:06:08 -0700

    Add Package.swift to ingest ComScore, and distribute this integration as a swift package

---
+++ Package.swift
@@ -0,0 +1,36 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "mParticle-ComScore",
+    platforms: [ .iOS(.v9) ],
+    products: [
+        .library(
+            name: "mParticle-ComScore",
+            targets: ["mParticle-ComScore"]),
+    ],
+    dependencies: [
+      .package(
+          name: "mParticle-Apple-SDK",
+          url: "https://github.com/mParticle/mparticle-apple-sdk",
+          .upToNextMajor(from: "8.0.0")
+      ),
+      .package(
+          name: "ComScore",
+          url: "https://github.com/comScore/Comscore-Swift-Package-Manager",
+          .upToNextMajor(from: "6.6.0")
+      )
+    ],
+    targets: [
+        .target(
+            name: "mParticle-ComScore",
+            dependencies: [
+                .byName(name: "mParticle-Apple-SDK"),
+                .byName(name: "ComScore"),
+            ],
+            path: "mParticle-ComScore"
+        )
+    ]
+)
